b74041760023df863b3132a9aba23c15afbfbb1e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2021-03-02 18:45:30 +0100

    handle error

--- lox/Lox.java
+++ lox/Lox.java
@@ -47,4 +47,14 @@ public class Lox {
     }
   }
 
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  private static void report(int line, String where,
+                             String message) {
+    System.err.println(
+        "[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
+  }
 }
